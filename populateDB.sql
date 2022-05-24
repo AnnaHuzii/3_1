388f9e20de51983b58c46d9a08d5a3513d88eea6
AnnaHuzii <94933305+AnnaHuzii@users.noreply.github.com>
2022-05-24 15:13:11 +0300

    hw_m31_v3

--- populateDB.sql
+++ populateDB.sql
@@ -24,19 +24,19 @@ INSERT INTO customers (name, EDRPOU, product) VALUES
 ('BI Group - Engineering', '160540301', 'ERP');
 
 INSERT INTO developers (full_name, birth_date, sex, email, skype) VALUES
-('Нерода Дмитрий Александрович', '1982-05-09', 'male', 'Neroda.D@inagro.com.ua', 'd.ramoss'),
-('Кравец Aндрей Евгеньевич', '1975-01-14', 'male', 'Kravets.A@inagro.com.ua', 'acrom_sc'),
-('Спивак Галина Владимировна', '1972-07-12', 'female', 'Spivak.G@inagro.com.ua', 'gala_spivak'),
-('Демченко Богдан Александрович', '1992-11-13', 'male', 'demchenko.b@ukr.net', 'bogdan_d6'),
-('Гузий Анна Александровна', '1984-07-08', 'female', 'anna_guzii@ukr.net', 'anna_guzii'),
-('Онищенко Елена Ивановна', '1990-01-02', 'female', 'onishchenko.l@inagro.com.ua', 'lena.stadnik32');
+('Neroda Dmitry', '1982-05-09', 'male', 'Neroda.D@inagro.com.ua', 'd.ramoss'),
+('Kravets Andrey', '1975-01-14', 'male', 'Kravets.A@inagro.com.ua', 'acrom_sc'),
+('Spivak Galina', '1972-07-12', 'female', 'Spivak.G@inagro.com.ua', 'gala_spivak'),
+('Demchenko Bogdan', '1992-11-13', 'male', 'demchenko.b@ukr.net', 'bogdan_d6'),
+('Huzii Anna', '1984-07-08', 'female', 'anna_guzii@ukr.net', 'anna_guzii'),
+('Onishchenko Elena', '1990-01-02', 'female', 'onishchenko.l@inagro.com.ua', 'lena.stadnik32');
 
 INSERT INTO project (name, description, company_id, customer_id) VALUES
 ('Казахстан БиАй-Групп', 'ERP з доробкою для управління бізнесом будівельної компанії', 1, 5),
-('УкрАгроКом', '', 1, 2),
-('КУП Агро', '', 1, 3),
-('Квартал', '', 2, 4),
-('Управління АГРО-бізнесом для України', '', 2, 4);
+('УкрАгроКом', 'Програма для компанії УкрАгроКом', 1, 2),
+('КУП Агро', 'Програма для комплекснго управління сільськогосподарським підприємством', 1, 3),
+('Квартал', 'Програма для комунальних підприємств', 2, 4),
+('Управління АГРО-бізнесом для України', 'Програма для управління сільськогосподарським підприємством', 2, 4);
 
 INSERT INTO developers_skills (developer_id, skill_id) VALUES
 (1,12),
